6d4d3d7c2be4b850a52ac8bb009d0d48ccfd6be0
Sam Gomez <Sam.Gomez@geneca.com>
2024-01-09 17:01:02 -0500

    Updated webAppName

--- iac/terraform/Part2/modules/appConfiguration/appConfiguration.tf
+++ iac/terraform/Part2/modules/appConfiguration/appConfiguration.tf
@@ -1,7 +1,7 @@
 data "azurerm_client_config" "current" {}
 
 data "azurerm_windows_web_app" "cm_webapp" {
-  name                = "${var.webAppName}${var.uniqueIdentifier}"
+  name                = "${var.webAppName}-${var.uniqueIdentifier}"
   resource_group_name = var.resourceGroupName
 }
 
