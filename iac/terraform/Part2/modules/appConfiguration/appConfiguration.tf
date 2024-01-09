10309079cc4842ee50e125efaf2916a1c36d2d74
Sam Gomez <Sam.Gomez@geneca.com>
2024-01-09 16:56:54 -0500

    Fix webAppName.

--- iac/terraform/Part2/modules/appConfiguration/appConfiguration.tf
+++ iac/terraform/Part2/modules/appConfiguration/appConfiguration.tf
@@ -1,7 +1,7 @@
 data "azurerm_client_config" "current" {}
 
 data "azurerm_windows_web_app" "cm_webapp" {
-  name                = var.webAppName
+  name                = "${var.webAppName}${uniqueIdentifier}"
   resource_group_name = var.resourceGroupName
 }
 
